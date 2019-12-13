139b0b7289d0d3c1673627d5a9bdbe171138f2d0
Jack Boeri <53635108+JekBurri@users.noreply.github.com>
2019-12-13 13:33:57 -0800

    Add files via upload
    Added a style.css for basic aesthetics. Still a major WIP.

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
 </head>
 <body>
-	<form action="login.php" method="post">
-		<input name="username" type="text"> <br>
-		<input name="password" type="password">
-		<input type="submit">
-	</form>
+	<div id="form">
+		<form action="login.php" method="post">
+			<br>
+			Username: <input name="username" type="text"> <br><br>
+			Password: <input name="password" type="password"> <br><br>
+			<input value="Login" type="submit">
+			
+		</form>
+	</div>
 </body>
 </html>
